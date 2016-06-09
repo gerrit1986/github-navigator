439e44840bf91de3f79050cd50d25a68dbcc7002
Gerrit <gerrit1986@gmail.com>
2016-06-09 19:51:25 +0200

    remove application.py

--- application.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-"""
-Launches a development server on localhost. You can look at this address in your browser to get the results.
-"""
-
-sys.argv = ["runserver"]
-
-execfile("manage.py")
